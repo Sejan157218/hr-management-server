ce0a2d9f19aa827357720a77e926c9095219b390
Sejan Mahmud <sejan157218@gmail.com>
2022-01-11 22:17:53 +0600

    connect with server

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const mysql=require("mysql");
-
+require('dotenv').config();
 const db=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:"",
-    database:'employeelist',
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_DATABASE,
 });
 db.connect((err)=>{
     if(err){
